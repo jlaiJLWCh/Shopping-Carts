2213f0603ab20d9b2415214a39fcdee900493d79
jlaiJLWCh <john910617850331@gmail.com>
2019-06-19 13:49:08 +0800

    建立會員管理後台 增加會員編輯頁面

---
+++ Shopping-Cart/Controllers/ManageUserController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shopping_Cart.Controllers
+{
+    public class ManageUserController : Controller
+    {
+        // GET: ManageUser
+        public ActionResult Index()
+        {
+            ViewBag.ResultMessage = TempData["ResultMessage"];
+
+            using (Models.UserEntities db = new Models.UserEntities())
+            {
+                var result = (from s in db.AspNetUsers
+                              select new Models.ManageUser
+                              {
+                                  Id = s.Id,
+                                  UserName = s.UserName,
+                                  Email = s.Email
+                              }).ToList();
+                return View(result);
+            }
+        }
+
+        public ActionResult Edit(string id)
+        {
+            using(Models.UserEntities db= new Models.UserEntities())
+            {
+                var result = (from s in db.AspNetUsers
+                              select new Models.ManageUser
+                              {
+                                  Id = s.Id,
+                                  UserName = s.UserName,
+                                  Email = s.Email
+                              }).FirstOrDefault();
+            }
+            return View();
+        }
+
+        public ActionResult Edit(Models.ManageUser postback)
+        {
+            using (Models.UserEntities db = new Models.UserEntities())
+            {
+                var result = (from s in db.AspNetUsers where s.Id == postback.Id select s).FirstOrDefault();
+                if(result != default(Models.AspNetUser))
+                {
+                    result.UserName = postback.UserName;
+                    result.Email = postback.Email;
+                    db.SaveChanges();
+
+                    TempData["ResultMessage"] = String.Format("使用者[{0}]成功編輯", postback.UserName);
+                    return RedirectToAction("Index");
+                }
+            }
+
+            TempData["ResultMessage"] = String.Format("使用者[{0}]不存在，請重新操作", postback.UserName);
+            return RedirectToAction("Index");
+        }
+    }
+}
