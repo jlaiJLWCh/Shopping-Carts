7cdcd1b6af4bc6345d68c66826071e81d4b34d6f
jlaiJLWCh <john910617850331@gmail.com>
2019-06-19 21:54:15 +0800

    新增  訂單管理相關功能

---
+++ Shopping-Cart/Controllers/ManageOrderController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shopping_Cart.Controllers
+{
+    public class ManageOrderController : Controller
+    {
+        // GET: ManageOrder
+        public ActionResult Index()
+        {
+            Models.ShoppingCartEntities db = new Models.ShoppingCartEntities();
+            var result = (from s in db.Orders select s).ToList();
+
+            return View(result);
+        }
+
+
+        public ActionResult Detail(int id)
+        {
+            using(Models.ShoppingCartEntities db = new Models.ShoppingCartEntities())
+            {
+                var result = (from s in db.OrderDetails where s.Id == id select s).ToList();
+
+                if(result.Count == 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(result);
+                }
+            }
+        }
+    }
+}
